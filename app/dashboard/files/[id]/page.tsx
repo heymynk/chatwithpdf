1a196f0ff2bb9dcf89c56db0a2bd223deba055ac
Hey-Mynk <mail.contactmayank@gmail.com>
2024-08-26 16:11:27 +0530

    push

---
+++ app/dashboard/files/[id]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function chatToFilePage() {
+  return (
+    <div>chatToFilePage</div>
+  )
+}
+
+export default chatToFilePage
--- components/FileUploader.tsx
+++ components/FileUploader.tsx
@@ -91,28 +91,28 @@ function FileUploader() {
 
       {!uploadProgress && (
         <div
-        {...getRootProps()}
-        className={`p-6 sm:p-10 border-2 border-dashed mt-10 w-full sm:w-[90%] border-purple-600 text-purple-600 rounded-lg h-80 sm:h-96 flex items-center justify-center transition-colors ${
-          isFocused || isDragAccept ? "bg-purple-300" : "bg-purple-100"
-        }`}
-      >
-        <input {...getInputProps()} />
-        <div className="flex flex-col justify-center items-center">
-          {isDragActive ? (
-            <>
-              <RocketIcon className="h-20 w-20 animate-bounce " />
-              <p className="text-sm sm:text-base">Drop the files here ...</p>
-            </>
-          ) : (
-            <>
-              <CircleArrowDown className="h-20 w-20 animate-bounce " />
-              <p className="text-sm sm:text-base">
-                Drag n drop some files here, or click to select files
-              </p>
-            </>
-          )}
+          {...getRootProps()}
+          className={`p-6 sm:p-10 border-2 border-dashed mt-10 w-full sm:w-[90%] border-purple-600 text-purple-600 rounded-lg h-80 sm:h-96 flex items-center justify-center transition-colors ${
+            isFocused || isDragAccept ? "bg-purple-300" : "bg-purple-100"
+          }`}
+        >
+          <input {...getInputProps()} />
+          <div className="flex flex-col justify-center items-center">
+            {isDragActive ? (
+              <>
+                <RocketIcon className="h-20 w-20 animate-bounce " />
+                <p className="text-sm sm:text-base">Drop the files here ...</p>
+              </>
+            ) : (
+              <>
+                <CircleArrowDown className="h-20 w-20 animate-bounce " />
+                <p className="text-sm sm:text-base">
+                  Drag n drop some files here, or click to select files
+                </p>
+              </>
+            )}
+          </div>
         </div>
-      </div>
       )}
     </div>
   );
