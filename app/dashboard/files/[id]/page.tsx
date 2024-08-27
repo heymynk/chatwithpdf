158b97ddf00d7b6ab7c2df0af0470a6b1ce45502
Hey-Mynk <mail.contactmayank@gmail.com>
2024-08-27 18:24:54 +0530

    chat page commit

--- app/dashboard/files/[id]/page.tsx
+++ app/dashboard/files/[id]/page.tsx
@@ -1,11 +1,40 @@
-function chatToFilePage({
+import PdfView from "@/components/PdfView";
+import { adminDb } from "@/firebaseAdmin";
+import { auth } from "@clerk/nextjs/server";
+
+async function chatToFilePage({
   params: { id },
 }: {
   params: {
     id: string;
   };
 }) {
-  return <div>chatToFilePage {id}</div>;
+  auth().protect();
+  const { userId } = await auth();
+
+  const ref = await adminDb
+    .collection("users")
+    .doc(userId!)
+    .collection("files")
+    .doc(id)
+    .get();
+
+  const url = ref.data()?.downloadUrl;
+
+  return (
+    <div className="grid lg:grid-cols-5 h-full overflow-hidden">
+      {/* Right */}
+      <div className="col-span-5 lg:col-span-2 overflow-y-auto">
+        {/* chat */}
+      </div>
+
+      {/* left */}
+      <div className="col-span-5 lg:col-span-3 bg-gray-100 border-r-2 lg:border-purple-600 lg:-order-1 overflow-auto">
+        {/* PDFView */}
+        <PdfView url={url} />
+      </div>
+    </div>
+  );
 }
 
 export default chatToFilePage;
